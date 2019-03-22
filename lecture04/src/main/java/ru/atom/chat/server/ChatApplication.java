7520eac2e26bfe016bcd396e6ad1aa2d84cc9c89
cepera1029 <sergey7182@ya.ru>
2019-03-22 09:24:00 +0300

    first start

--- lecture04/src/main/java/ru/atom/chat/server/ChatApplication.java
+++ lecture04/src/main/java/ru/atom/chat/server/ChatApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatApplication {
+    //comment
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
     }
